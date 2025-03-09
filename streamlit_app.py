e6785c9d72de15938c240124881b9f3da33fa989
phucbuudientrunguong <phucaibdtw@gmail.com>
2025-03-09 15:34:41 +0700

    đổi cau chào
    đổi

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,5 +95,6 @@ if prompt := st.chat_input("Bạn nhập nội dung cần trao đổi ở đây 
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
-##### xin chao
+##### xin chao Linh ở đây để giúp bạn
+
 
